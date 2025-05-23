279a5503624d536043a6630d1d17285f69dabce4
Brenton Unger II <bunger@buildableworks.com>
2025-05-23 08:44:32 -0700

    create an org with team members

--- functions/src/models/domain/organization.ts
+++ functions/src/models/domain/organization.ts
@@ -1,6 +1,12 @@
+import { TeamMember } from "./team-member";
+
 /** An organization (your customer) in pure domain terms */
 export interface Organization {
     name: string;
     dateCreated: Date;
     dateUpdated: Date;
+}
+
+export interface OrganizationWithTeamMembers extends Organization {
+    teamMembers: TeamMember[];
 }

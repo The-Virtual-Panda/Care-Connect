b51692ad097441cff62773bba453253454b00864
Brenton Unger II <bunger@buildableworks.com>
2025-05-23 10:57:33 -0700

    add emulator support

--- functions/src/firebase-config.ts
+++ functions/src/firebase-config.ts
@@ -1,7 +1,10 @@
 import { getApps, initializeApp } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
+import { logger } from "firebase-functions";
 
+// Detect if running locally with emulator
+const isEmulator = process.env.USE_EMULATOR === "true";
 
 // Initialize the default app *once*
 if (!getApps().length) {
@@ -10,3 +13,12 @@ if (!getApps().length) {
 
 export const fireDb = getFirestore();
 export const auth = getAuth();
+
+// Connect to Firestore emulator if FIRESTORE_EMULATOR_HOST is set
+if (isEmulator) {
+    logger.info("Connecting to Firestore emulators...");
+    fireDb.settings({
+        host: process.env.FIRESTORE_EMULATOR_HOST,
+        ssl: false,
+    });
+}

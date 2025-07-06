1124b4759e6e6469f6faf11eae966f30b82320dd
Brenton Unger II <bunger@buildableworks.com>
2025-07-06 13:24:57 -0700

    re-add firebase

--- client/src/app.config.ts
+++ client/src/app.config.ts
@@ -6,6 +6,9 @@ import Aura from '@primeng/themes/aura';
 import { providePrimeNG } from 'primeng/config';
 import { appRoutes } from './app.routes';
 import { definePreset } from '@primeng/themes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { connectAuthEmulator, getAuth, provideAuth } from '@angular/fire/auth';
+import { connectFirestoreEmulator, getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { environment } from './environments/environment';
 
 const MyPreset = definePreset(Aura, {
@@ -82,6 +85,22 @@ export const appConfig: ApplicationConfig = {
         provideAnimationsAsync(),
 
         providePrimeNG({ theme: { preset: MyPreset, options: { darkModeSelector: '.app-dark' } } }),
+
+        provideFirebaseApp(() => initializeApp(environment.firebase)),
+        provideAuth(() => {
+            const auth = getAuth();
+            if (environment.useEmulators) {
+                connectAuthEmulator(auth, "http://localhost:9099", { disableWarnings: true });
+            }
+            return auth;
+        }),
+        provideFirestore(() => {
+            const firestore = getFirestore();
+            if (environment.useEmulators) {
+                connectFirestoreEmulator(firestore, "localhost", 8080);
+            }
+            return firestore;
+        })
     ]
 };
 
